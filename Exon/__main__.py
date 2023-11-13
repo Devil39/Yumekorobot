08582b3c878ba8329e4034bb5d7abb3a523bfb95
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-11-13 08:28:26 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -36,9 +36,9 @@ from Exon.modules.helper_funcs.misc import paginate_modules
 from Exon.modules.language import gs
 
 """
-PM_START_TEX = Hᴇʟʟᴏ `{}`, ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ \nᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ ʙʀᴏ . ..
+PM_START_TEX = Hᴇʟʟᴏ `{}`, ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ \nᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ ʙʀᴏ . .. """
 START_IMG = "https://graph.org//file/84e16ca7607cc8fddd8bb.jpg"
-"""
+
 buttons = [
     [
         InlineKeyboardButton(
