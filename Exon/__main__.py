04b090317140a1b8adafb305131c5191e35d5e9d
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-11-14 09:33:27 +0530

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -256,7 +256,7 @@ update.effective_message.reply_photo(
     reply_markup=InlineKeyboardMarkup(buttons),
     parse_mode=ParseMode.HTML,
     timeout=60,
-)
+),
 else:
     update.effective_message.reply_text(gs(chat.id, "grp_start_text"))
 
